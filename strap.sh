851bf714e0a76bb72e796f3b23d4ed5737b219fe
Lion Ackermann <ackerm.lion@hotmail.de>
2020-07-23 22:26:52 +0200

    Add keystone, docker, qira

--- strap.sh
+++ strap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OLD_PWD=$(pwd)
+
 sudo apt update --fix-missing
 sudo apt upgrade -y
 
@@ -7,7 +9,12 @@ sudo apt upgrade -y
 
 sudo apt install build-essential module-assistant
 read -p "Path to VirtualBox Guest Additions: " VBOX_GUEST_PATH
-sudo bash $VBOX_GUEST_PATH/VBoxLinuxAdditions.run
+if [ ! -z "$VBOX_GUEST_PATH" ]; then 
+    sudo bash $VBOX_GUEST_PATH/VBoxLinuxAdditions.run
+else
+    echo "Skipping VirtualBox Guest Additions."
+fi
+
 
 # Install tools
 
@@ -32,7 +39,17 @@ sudo apt install -y \
     python3-dev \
     ruby \
     gdbserver \
-    netcat
+    netcat \
+    gnupg-agent
+
+# Docker for backwards easier backwards compatiblity and stuff
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
 
 
 # Some common python tools for CTF stuff
@@ -40,10 +57,30 @@ pip3 install --upgrade pip
 pip3 install virtualenv httpx angr cryptography unicorn ropper capstone mitmproxy ipython
 python3 -m pip install --upgrade git+https://github.com/Gallopsled/pwntools.git@dev
 wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
+
+# install keystone. uff
+cd /tmp
+git clone https://github.com/keystone-engine/keystone.git 
+cd keystone 
+mkdir build && cd build 
+../make-share.sh
+sudo make install 
+sudo ldconfig 
+cd ../bindings/python 
+sudo python3 setup.py install
+cd $OLD_PWD
+
 sudo gem install one_gadget
 
-# Custom utility 
 (mkdir $HOME/.local || : ) 2> /dev/null
+
+# qira
+git clone https://github.com/geohot/qira.git $HOME/qira
+cd $Home/qira
+sudo docker build -t qira -f docker/Dockerfile .
+cd $OLD_PWD
+
+# Custom utility 
 git clone https://github.com/liona24/utility-scripts.git $HOME/.local/utility-scripts
 
 # configure VIM
